ecdfdbd26f153be166b0f1add6ead9a1abee5681
maayankfir <mayankfir@gmail.com>
2018-06-17 13:49:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function takeANumber (katzDeliLine, customerName ) {
+  katzDeliLine.push (customerName) ;
+  return "Welcome, " + customerName + ". You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing (line) {
+  if (0 === line) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = line[0]
+    line.splice (0,1)
+    return "Currently serving" + name + '.' ;
+  }
+}
+
+function currentLine (katzDeliLine) {
+  var katzDeliLine = []
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for {var i=0; i < katzDeliLine.length; i++ }
+    katzDeliLine += (i + 1) + "." + katzDeliLine[i] + ","
+  }
+  katzDeliLine = line.slice (0, katzDeliLine.length-2)
+  return "The line is currently: " + katzDeliLine
+  }
+}
